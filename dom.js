729ce8e88a85a1e585bdba4acdf37c43e3ada5c0
Abhijnan Acharya <33322551+abhijnanacharya@users.noreply.github.com>
2021-11-04 01:15:06 +0530

    Dom Functionality Assignment added

---
+++ dom.js
@@ -0,0 +1,8 @@
+var btn=document.querySelector('input[type="submit"]');
+btn.value="SEND";
+let ttl=document.getElementsByClassName("title");
+ttl.innerHTML="<b>ADD ITEM</b>";
+btn.style.color='green';
+let ttl2=document.querySelector(".title");
+ttl2.style.color='green';
+document.querySelector('.title').style.border="12px solid green";
