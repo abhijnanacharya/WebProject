5914f82e596a6abde1271ea0e0bc2700048fd583
Abhijnan Acharya <33322551+abhijnanacharya@users.noreply.github.com>
2021-11-07 14:21:52 +0530

    DOM APPEND NODES ADDED

--- script.js
+++ script.js
@@ -56,4 +56,39 @@ for(let i=0; i<document.getElementsByTagName('li').length; i++){
     let lis=document.getElementsByTagName('li');
     lis[i].textContent ='HELLO WORLD';
 }
+var itemList=document.querySelector('.items');
+//console.log(itemList);
+itemList.parentNode.style.backgroundColor = '#f4f4f4';
 }
+//createElement
+var newDiv=document.createElement('div');
+console.log(newDiv);
+newDiv.className='hello';
+newDiv.id="hello1";
+newDiv.setAttribute('title', 'Hello Div');
+//Create a text Node with the
+let newDivText=document.createTextNode("Hello World");
+//Add txt to Div
+newDiv.appendChild(newDivText);
+
+//createElement2
+var newDiv2=document.createElement('div2');
+console.log(newDiv2);
+newDiv.className='hello2';
+newDiv.id="hello22";
+newDiv.setAttribute('title2', 'Hello Div2');
+//Create a text Node with the
+let newDivText2=document.createTextNode("Hello World 222");
+//Add txt to Div
+newDiv2.appendChild(newDivText2);
+
+let container=document.querySelector('header.container');
+console.log(container);
+let h1=document.querySelector('header h1');
+console.log(h1);
+container.insertBefore(newDiv, h1);
+let container2=document.querySelector('ul.items');
+console.log(container2);
+let h2=document.querySelector('#myitem1');
+console.log(h2);
+container2.insertBefore(newDiv2,h2);
