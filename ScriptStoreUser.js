87778319a199e13e7f2fa39ee3eaaccedf73b055
Abhijnan Acharya <33322551+abhijnanacharya@users.noreply.github.com>
2021-11-08 18:20:23 +0530

    DOMContentLoaded ADDED

--- ScriptStoreUser.js
+++ ScriptStoreUser.js
@@ -7,20 +7,44 @@ function addItem(e){
     let userRecords=new Array();
     var newName = document.getElementById('name').value;
     var newEmail = document.getElementById('email').value;
-    userRecords=JSON.parse(localStorage.getItem('records'))?JSON.parse(localStorage.getItem('records')):[];
-    if(userRecords.some((v)=>{return v.email==newEmail})){
-        alert('APPOINTMENT ALREADY PRESENT WITH THE ENTERED EMAIL!!');
-    }else{
-        userRecords.push({
-            "name":newName,
-            "email":newEmail,
-        });
+    //userRecords=JSON.parse(localStorage.getItem('records'))?JSON.parse(localStorage.getItem('records')):[];
+    //if(userRecords.some((v)=>{return v.email==newEmail})){
+      //  alert('APPOINTMENT ALREADY PRESENT WITH THE ENTERED EMAIL!!');
+    //}else{
+        const obj={
+           /* "name":*/newName,
+            /*"email":*/newEmail,
+        };
     
         var li = document.createElement('li');
    
         li.className = 'list-group-item';
         li.appendChild(document.createTextNode(newName+" "+newEmail));
         UserList.appendChild(li);
-        localStorage.setItem("records",JSON.stringify(userRecords));
-    }
+        localStorage.setItem(obj.newEmail,JSON.stringify(obj));
+    //}
+
+}
+
+document.addEventListener("DOMContentLoaded",Load());
+
+function Load() {
+    Object.keys(localStorage).forEach((key) => {
+        stringifiedDetailsOfPeople = localStorage.getItem(key);
+        namesofUsers = JSON.parse(stringifiedDetailsOfPeople);
+        
+
+
+        var li = document.createElement('li');
+   
+        li.className = 'list-group-item';
+        li.appendChild(document.createTextNode((namesofUsers.newName)+" "+key));
+        UserList.appendChild(li);
+        
+        
+        
+        
+        
+        });
+
 }
