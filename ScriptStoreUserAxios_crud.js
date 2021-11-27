2cc8a0a3b8103f5199bed951a61b64751856a3f2
abhijnanacharya <33322551+abhijnanacharya@users.noreply.github.com>
2021-11-27 14:37:11 +0530

    GetUser Fucntionality added from CrudCrud

--- ScriptStoreUserAxios_crud.js
+++ ScriptStoreUserAxios_crud.js
@@ -1,5 +1,3 @@
-//const { default: axios } = require("axios");
-
 var form = document.getElementById('myForm');
 var UserList = document.getElementById('users');
 form.addEventListener('submit',addItem);
@@ -11,17 +9,31 @@ function addItem(e){
     let userRecords=new Array();
     var newName = document.getElementById('name').value;
     var newEmail = document.getElementById('email').value;
-    //userRecords=JSON.parse(localStorage.getItem('records'))?JSON.parse(localStorage.getItem('records')):[];
-    //if(userRecords.some((v)=>{return v.email==newEmail})){
-      //  alert('APPOINTMENT ALREADY PRESENT WITH THE ENTERED EMAIL!!');
-    //}else{
         const obj={
-           /* "name":*/newName,
-            /*"email":*/newEmail,
+            newName,
+            newEmail,
         };
     axios.post("https://crudcrud.com/api/95cdf2e008cb4fd5b4c6c3ec562459cb/UserDat",obj)
     .then(response => {
-        var li = document.createElement('li');
+        showDetails(response.data);
+    })
+    .catch(error => console.error(`Something went wrong: ${error.message}`));
+    }
+        
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+      if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        console.log(li);
+        UserList.removeChild(li);
+        //localStorage.removeItem(e.newEmail);
+      }
+    }
+  }
+
+function showDetails(response){
+    console.log(response);
+    var li = document.createElement('li');
         // Create del button element
         var deleteBtn = document.createElement('button');
 
@@ -36,51 +48,24 @@ function addItem(e){
         
    
         li.className = 'list-group-item';
-        li.appendChild(document.createTextNode(obj.newName+" "+obj.newEmail));
+        li.appendChild(document.createTextNode(response.newName+" "+response.newEmail));
         li.appendChild(deleteBtn);
         UserList.appendChild(li);
-    })
-    .catch(error => console.error("Something went wrong"));
-    }
-        
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-      if(confirm('Are You Sure?')){
-        var li = e.target.parentElement;
-        console.log(li);
-        UserList.removeChild(li);
-        //localStorage.removeItem(e.newEmail);
-      }
-    }
-  }
-
-document.addEventListener("DOMContentLoaded",Load());
-
-function Load() {
-    Object.keys(localStorage).forEach((key) => {
-        stringifiedDetailsOfPeople = localStorage.getItem(key);
-        namesofUsers = JSON.parse(stringifiedDetailsOfPeople);
-        
-
-
-        var li = document.createElement('li');
-   
-        li.className = 'list-group-item';
-        li.appendChild(document.createTextNode((namesofUsers.newName)+" "+key));
-         // Create del button element
-         var deleteBtn = document.createElement('button');
-
-         // Add classes to del button
-         deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
- 
-         // Append text node
-         deleteBtn.appendChild(document.createTextNode('X'));
- 
-         // Append button to li
-         li.appendChild(deleteBtn);
-        UserList.appendChild(li);
-        
-        });
 
 }
+
+document.addEventListener("DOMContentLoaded",()=> {
+    axios.get("https://crudcrud.com/api/95cdf2e008cb4fd5b4c6c3ec562459cb/UserDat")
+    .then(response=>{
+        for(var i=0; i<response.data.length; i++){
+            showDetails(response.data[i]);
+        }
+    })
+    .catch(err=>console.error(err));
+});
+
+
+      
+
+
 
