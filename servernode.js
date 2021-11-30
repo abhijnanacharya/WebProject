3a42967797b4621b88ad6ca32d60f8f052ac3fd3
Abhijnan Acharya <33322551+abhijnanacharya@users.noreply.github.com>
2021-12-01 01:15:07 +0530

    Webpage Directory Assignment added

--- servernode.js
+++ servernode.js
@@ -1,6 +1,13 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log("Hello World!");
+    let url=req.url;
+    if(url==='/home'){
+        res.write('<html><head><h1>WELCOME HOME!</h1></head></html>')
+    }else if(url==='/about'){
+        res.write('<html><head><h1>WELCOME TO ABOUT US PAGE!</h1></head></html>')
+    }if(url==='/node'){
+        res.write('<html><head><h1>WELCOME MY NODE Js PROJECT!</h1></head></html>')
+    }
 });
-server.listen(4000);
+server.listen(8080);
